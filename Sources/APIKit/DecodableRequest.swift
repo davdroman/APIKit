adc03d4329aeac23a5c0961399c42d4570ac3b02
davdroman <d@vidroman.dev>
2020-03-14 18:58:09 +0000

    Implement `DecodableRequest` protocol

---
+++ Sources/APIKit/DecodableRequest.swift
@@ -0,0 +1,38 @@
+// TODO: documentation
+
+import Foundation
+#if canImport(Combine)
+import Combine
+
+@available(iOS 13, *)
+public protocol DecodableRequest: RawJSONRequest where Response: Decodable {
+    associatedtype Error: Swift.Error, Decodable
+    associatedtype Decoder: TopLevelDecoder where Decoder.Input == Data
+    var decoder: Decoder { get }
+}
+
+@available(iOS 13, *)
+public extension DecodableRequest {
+    func intercept(object: Data, urlResponse: HTTPURLResponse) throws -> Data {
+        guard 200..<300 ~= urlResponse.statusCode else {
+            let parsedError = try? decoder.decode(Error.self, from: object)
+            throw parsedError ?? NSError(
+                domain: "",
+                code: -1,
+                userInfo: [NSLocalizedDescriptionKey: HTTPURLResponse.localizedString(forStatusCode: urlResponse.statusCode).capitalized(with: .current)]
+            )
+        }
+        return object
+    }
+
+    func response(from object: Data, urlResponse: HTTPURLResponse) throws -> Response {
+        try decoder.decode(Response.self, from: object)
+    }
+}
+
+public protocol DecodableJSONRequest: DecodableRequest where Decoder == JSONDecoder {}
+
+public extension DecodableJSONRequest {
+    var decoder: Decoder { JSONDecoder() }
+}
+#endif
